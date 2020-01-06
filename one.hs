ea68c3ec4361f815cbcac6635527ad5fb79dfebe
turbobeast <georgemichaelfaust@gmail.com>
2020-01-05 21:09:17 -0500

    recursively calculating the fuel

--- one.hs
+++ one.hs
@@ -1,6 +1,12 @@
 fuel :: Integer -> Integer
 fuel = (\x -> x - 2) . (\x -> x `div` 3)
 
+calculateFuel :: Integer -> Integer
+calculateFuel x
+        | fuel' <= 0 = 0
+        | otherwise = fuel' + calculateFuel fuel'
+    where fuel' = fuel x
+
 parseInput :: String -> [Integer]
 parseInput = (map read) . lines
 
@@ -8,5 +14,5 @@ main :: IO ()
 main = do
     rawData <- getContents
     let parsedInput = parseInput rawData
-        totalFuel =  (sum . map fuel) parsedInput
+        totalFuel =  (sum . map calculateFuel) parsedInput
     putStrLn (show totalFuel)
